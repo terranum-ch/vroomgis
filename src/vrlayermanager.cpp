ea4820f49b34dc753fc49102134e97b555e7dfca
Lucien Schreiber <lucien.schreiber@gmail.com>
2009-11-27 17:55:01 +0000

    Adding code for vrLayerManager

---
+++ src/vrlayermanager.cpp
@@ -0,0 +1,31 @@
+/***************************************************************************
+								vrlayermanager.cpp
+				Manage the layers. Keep a list of all opened layers
+                             -------------------
+    copyright            : (C) 2009 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "vrlayermanager.h"
+
+
+
+wxArrayString vrLayerManager::GetDriversExtension() 
+{
+}
+
+
+
+wxString vrLayerManager::GetDriversWildcards() 
+{
+}
