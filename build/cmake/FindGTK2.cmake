98dfa8f33c116463015dd8df4e380a398439fcf6
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2010-01-08 14:06:22 +0000

    Adding scripts for building wxWidgets and test automatically

--- build/cmake/FindGTK2.cmake
+++ build/cmake/FindGTK2.cmake
@@ -24,7 +24,71 @@ set( CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/CMakeModules")
 # Include FindGLib
 
 set( GLIB_FIND_REQUIRED ${GTK2_FIND_REQUIRED})
-include( "cmake/FindGLib.cmake")
+
+# Locate GLib files
+
+if( NOT GLIB_FOUND)
+
+	find_path( GLIB_INCLUDE_DIR glib.h 
+		PATHS /opt/gtk/include
+				/opt/gnome/include
+				/usr/include
+				/usr/local/include
+		PATH_SUFFIXES glib-2.0
+		NO_DEFAULT_PATH
+	)
+	MARK_AS_ADVANCED( GLIB_INCLUDE_DIR)
+		
+	find_path( GLIB_CONFIG_INCLUDE_DIR glibconfig.h 
+		PATHS	/opt/gtk/include
+				/opt/gtk/lib
+				/opt/gnome/include 
+				/opt/gnome/lib
+				/usr/include
+				/usr/lib
+				/usr/local/include
+		PATH_SUFFIXES /glib-2.0/include
+		NO_DEFAULT_PATH
+	)
+	MARK_AS_ADVANCED( GLIB_CONFIG_INCLUDE_DIR)
+
+	if( NOT GLIB_CONFIG_INCLUDE_DIR)
+		message( STATUS "Unable to find GLIB_CONFIG_INCLUDE_DIR")
+	endif( NOT GLIB_CONFIG_INCLUDE_DIR)
+		
+	find_library( GLIB_LIBRARY 
+		NAMES glib-2.0 
+		PATHS /opt/gtk/lib
+				/opt/gnome/lib
+				/usr/lib 
+				/usr/local/lib
+		NO_DEFAULT_PATH
+	) 
+	MARK_AS_ADVANCED( GLIB_LIBRARY)
+		
+	if( GLIB_INCLUDE_DIR AND GLIB_CONFIG_INCLUDE_DIR AND GLIB_LIBRARY)
+		set( GLIB_FOUND TRUE)
+		set( GLIB_INCLUDE_DIRS ${GLIB_INCLUDE_DIR} ${GLIB_CONFIG_INCLUDE_DIR})
+		set( GLIB_LIBRARIES ${GLIB_LIBRARY})
+	endif( GLIB_INCLUDE_DIR AND GLIB_CONFIG_INCLUDE_DIR AND GLIB_LIBRARY)
+	
+	if( GLIB_FOUND)
+		if( NOT GLIB_FIND_QUIETLY)
+			message( STATUS "Found GLib library: ${GLIB_LIBRARY}")
+			message( STATUS "Found GLib inc dirs: ${GLIB_INCLUDE_DIRS}")
+		endif( NOT GLIB_FIND_QUIETLY)
+	else( GLIB_FOUND)
+		if( GLIB_FIND_REQUIRED)
+			message( FATAL_ERROR "Could not find GLib")
+		else( GLIB_FIND_REQUIRED)
+			if( NOT GLIB_FIND_QUIETLY)
+				message( STATUS "Could not find GLib")
+			endif( NOT GLIB_FIND_QUIETLY)
+		endif( GLIB_FIND_REQUIRED)
+	endif( GLIB_FOUND)
+
+endif( NOT GLIB_FOUND)
+
 
 message( STATUS "Looking for GTK2 ...")
 
