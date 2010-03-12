6c94c6b4ef87418e94e91a7c6ca03b0756e5d4e3
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2010-03-12 09:48:06 +0000

    Adding class for computing intersection between rectangle with negative height and associated tests

---
+++ test/src/test_vrrealrect.h
@@ -0,0 +1,117 @@
+/***************************************************************************
+test_vrcoordinate.h
+Testing the coordinate system
+                             -------------------
+    copyright            : (C) 2009 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _TEST_VRREALRECT_H_
+#define _TEST_VRREALRECT_H_
+
+
+#include "wx/wxprec.h"
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include "test_param.h"	//for test parameters
+#include "../../src/vroomgis.h"
+#include "../../src/vrrealrect.h"
+
+
+
+
+class TEST_vrRealRect : public CxxTest::TestSuite
+{
+public:
+	void testShowTestName(){
+		wxLogMessage("******************************");
+		wxLogMessage(" TESTING vrRealRect CLASS     ");
+		wxLogMessage("******************************");
+	}
+	
+	void testCreateRealRect(){
+		vrRealRect myRect;
+		TS_ASSERT(myRect.IsOk() ==false);
+	}
+	
+	
+	
+	
+	void testIntersect1(){
+		vrRealRect myRectWnd (0, 1000, 4000, -1000);
+		vrRealRect myRectImg (1000, 800, 2000, -600);
+		
+		TS_ASSERT(myRectWnd.IsOk() == true);
+		TS_ASSERT(myRectImg.IsOk() == true);
+		
+		vrRealRect myIntersect = myRectWnd.Intersect(myRectImg);
+		wxLogMessage("Intersection computed : %.2f / %.2f / %.2f / %.2f",
+					 myIntersect.GetLeft(),
+					 myIntersect.GetTop(),
+					 myIntersect.GetRight(),
+					 myIntersect.GetBottom());
+		
+		TS_ASSERT(myIntersect == myRectImg);
+	}
+	
+	void testIntersect2(){
+		vrRealRect myRectWnd (0, 1000, 4000, -1000);
+		vrRealRect myRectImg (2000, 2000, 3000, -1500);
+		
+		vrRealRect myResult (2000, 1000 ,2000, -500);
+		
+		TS_ASSERT(myRectWnd.IsOk() == true);
+		TS_ASSERT(myRectImg.IsOk() == true);
+		
+		vrRealRect myIntersect = myRectWnd.Intersect(myRectImg);
+		wxLogMessage("Intersection computed : %.2f / %.2f / %.2f / %.2f",
+					 myIntersect.GetLeft(),
+					 myIntersect.GetTop(),
+					 myIntersect.GetRight(),
+					 myIntersect.GetBottom());
+		
+		TS_ASSERT(myIntersect == myResult);
+		
+	}
+	
+	
+	void testIntersect3(){
+		vrRealRect myRectWnd (1000, 1000, 4000, -1000);
+		vrRealRect myRectImg (0, 500, 2000, -1500);
+		
+		vrRealRect myResult (1000, 500 ,1000, -500);
+		
+		TS_ASSERT(myRectWnd.IsOk() == true);
+		TS_ASSERT(myRectImg.IsOk() == true);
+		
+		vrRealRect myIntersect = myRectWnd.Intersect(myRectImg);
+		wxLogMessage("Intersection computed : %.2f / %.2f / %.2f / %.2f",
+					 myIntersect.GetLeft(),
+					 myIntersect.GetTop(),
+					 myIntersect.GetRight(),
+					 myIntersect.GetBottom());
+		
+		TS_ASSERT(myIntersect == myResult);
+		
+	}
+
+};
+
+
+
+
+
+
+
+#endif
