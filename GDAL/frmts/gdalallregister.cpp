e266e62d9a39197dcbfa46c94dbd065a08dfde65
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2010-05-06 08:56:31 +0000

    All files are now tweaked for building c2d but building didn't pass because of c2ddataset.cpp lack of code

--- GDAL/frmts/gdalallregister.cpp
+++ GDAL/frmts/gdalallregister.cpp
@@ -435,6 +435,11 @@ void CPL_STDCALL GDALAllRegister()
 #ifdef FRMT_saga
     GDALRegister_SAGA();
 #endif
+	
+#ifdef FRMT_c2d
+	GDALRegister_C2D();
+#endif
+	
 /* -------------------------------------------------------------------- */
 /*      Deregister any drivers explicitly marked as supressed by the    */
 /*      GDAL_SKIP environment variable.                                 */
