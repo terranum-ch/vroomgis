ca49c95792b1f74cdd3454120e9b3819b1b9775e
Lucien Schreiber <lucien.schreiber@gmail.com>
2010-06-07 13:26:52 +0000

    Defining Pi if not defined (C2D driver)

--- GDAL/frmts/c2d/c2ddataset.cpp
+++ GDAL/frmts/c2d/c2ddataset.cpp
@@ -30,6 +30,9 @@
 #include "cpl_string.h"
 #include <ctype.h>
 
+#ifndef M_PI
+# define M_PI  3.1415926535897932384626433832795
+#endif
 
 // ------------------------------------------------------------------------
 // Constant for C2D formats
