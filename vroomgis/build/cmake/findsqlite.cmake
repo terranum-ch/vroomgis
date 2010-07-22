92d7dc6dec5802c261997566e080274da6abdc27
Lucien Schreiber <lucien.schreiber@gmail.com>
2010-07-22 16:35:40 +0000

    Adding support for SQLite

---
+++ vroomgis/build/cmake/findsqlite.cmake
@@ -0,0 +1,29 @@
+# Find SQLite Library
+# Created by Lucien Schreiber 
+#
+#  SQLITE_INCLUDE_DIR - where to find sqlite3.h, etc.
+#  SQLITE_LIBRARIES   - List of libraries when using sqlite.
+
+SET (SQLITE_PATH CACHE PATH "Path to the SQLite library")
+
+
+# SEARCHING HEADER
+FIND_PATH (SQLITE_INCLUDE_DIR sqlite3.h
+  ${SQLITE_PATH}
+  ${SQLITE_PATH}/include)
+
+IF(NOT SQLITE_INCLUDE_DIR)
+  MESSAGE(FATAL_ERROR "sqlite3.h file not found")
+ENDIF(NOT SQLITE_INCLUDE_DIR)
+INCLUDE_DIRECTORIES(${SQLITE_INCLUDE_DIR})
+
+# SEARCHING LIBRARY
+FIND_LIBRARY(SQLITE_LIBRARIES sqlite
+  ${SQLITE_PATH}
+  ${SQLITE_PATH}/lib)
+
+IF(NOT SQLITE_LIBRARIES)
+  MESSAGE(FATAL_ERROR "SQLite library not found")
+ENDIF(NOT SQLITE_LIBRARIES)
+MESSAGE("SQLite libraries : ${SQLITE_LIBRARIES}")
+LINK_LIBRARIES(${SQLITE_LIBRARIES})
