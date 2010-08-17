bfd08bbfc2b5cd3ab5e7011995a23b3b033955fc
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2010-08-17 08:49:06 +0000

    Changing TOC function to virtual

--- vroomgis/src/vrviewertoc.h
+++ vroomgis/src/vrviewertoc.h
@@ -43,8 +43,6 @@ private:
 	bool m_FreezeStatus;
 	
 	// event function
-	void OnVisibleStatusChanged(wxCommandEvent & event);
-
 	void OnMouseRightDown(wxMouseEvent & event);
     void OnMouseWheel(wxMouseEvent & event);
 	
@@ -59,6 +57,7 @@ private:
 	
 protected:
     void _ReloadData();
+	virtual void OnVisibleStatusChanged(wxCommandEvent & event);
 	virtual void _ShowMenuContextual(int id, vrRenderer * renderer);
 	
 	
