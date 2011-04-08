cc74901343db771ca28b3f40141f57eaccc5b33d
Lucien Schreiber <lucien.schreiber@gmail.com>
2011-04-08 11:06:23 +0000

    Adding Python build script for vroomloader, vroomtwin and unit tests

---
+++ scripts/macbook.py
@@ -0,0 +1,44 @@
+# VARIABLES FOR BUILDING ON OSX 
+
+# Variables to define, see update_vroomgis.py:
+# gwxWidgets = wxconfig binary
+# gwxWidgetsVersion = trunk of wxwidgets when using SVN version
+# gTrunk = Path to the vroomgis trunk
+# gDirBin = Path to the vroomgis binary dir (will contain : vroomloader, vroomtwin, vroomtests)
+# 
+# gDirGis = Path to the GIS directory (GDAL, GEOS, PROJ), may be empty if installed in standard directory
+# gDirTestData = Path to the test data, only needed for vroomgistests
+# gDirCxx = Path to the cxxtest library
+# gDirSQLite = Path to the sqlite library, may be empty if using standard library.
+
+gwxWidgets = "/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBWXSVN/bin/wx-config" #wxWIDGETS config
+gwxWidgetsVersion = "/Users/lucien/DATA/PROGRAMATION/_LIB/64/wxWidgets-svn/"
+gDirTrunk = "/Users/lucien/DATA/PRJ/COLTOP-GIS/trunk_vroomgis"
+gDirBin = "/Users/lucien/DATA/PRJ/COLTOP-GIS/bin/vroomgis"
+gDirGis = "/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS"
+gDirTestData = "/Users/lucien/DATA/PRJ/COLTOP-GIS/test_data"
+gDirCxx = "/Users/lucien/DATA/PROGRAMATION/_LIB/cxxtest"
+gDirSQLite = ""
+
+
+# CMAKE SPECIFIC
+gCmakeEnv = "Xcode" #result of cmake --help
+gCmakeSpecific = ['-DCMAKE_OSX_ARCHITECTURES:TEXT=x86_64']
+gCmakeSpecific.append("-DCMAKE_WXWINDOWS_WXCONFIG_EXECUTABLE:FILE=" + gwxWidgets)
+
+
+# PLATEFORM SPECIFIC FUNCTION
+#CONFIG SPECIFIC
+def gBeforeConfig():
+	print ("No special configurations")
+
+
+# BUILD SPECIFIC
+def gBuildCommand(buildtype="Debug", directory = ""):
+	if (buildtype == "Release"):
+		return ['xcodebuild', '-configuration', 'Release']
+	return ['xcodebuild', '-configuration', 'Debug']
+	
+
+
+	
