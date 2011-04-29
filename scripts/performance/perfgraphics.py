deb534bd905877775650953b0ec78c64f33980f1
Lucien Schreiber <lucien.schreiber@gmail.com>
2011-04-29 14:27:12 +0000

    Last version of performance script

--- scripts/performance/perfgraphics.py
+++ scripts/performance/perfgraphics.py
@@ -7,9 +7,27 @@ Created by Lucien Schreiber on 2011-04-28.
 Copyright (c) 2011 CREALP. All rights reserved.
 """
 
+"""
+Simple code for plotting:
+from pylab import *
+title ("vroomGIS performance: Debug VS Release")
+xlabel("Number of layers")
+ylabel("Loading time [ms]")
+...
+plot instructions
+...
+legend(loc="best")
+show()
+"""
+
+
 import sys
 import os
-from pylab import *
+try:
+	from pylab import *
+except:
+	print ("Fatal error, unable to load matplotlib!")
+	exit()
 
 
 def splitList(listoflist):
@@ -22,16 +40,26 @@ def splitList(listoflist):
 	return (listx, listy)
 
 
-
-perfilepath = "/Users/lucien/vroomgis-performance.txt"
-perfile = open(perfilepath, "r")
-mylines = perfile.readlines()
-perfile.close()
-
 print ("******************************************************")
 print ("*** vroomGIS performance, graphical representation ***")
 print ("***            (c) L. Schreiber, 2011              ***")
 print ("******************************************************")
+
+homedir = os.path.expanduser('~')
+perfilepath = homedir + os.sep + "vroomgis-performance.txt"
+
+if (raw_input("Use file: '{0}' (Y/N): ".format(perfilepath)).upper() != 'Y'):
+	perfilepath = raw_input("Enter file path and name: ")
+	
+try:
+	perfile = open(perfilepath, "r")
+except:
+	print("Error opening file: '{0}'".format(perfilepath))
+	exit()
+	
+mylines = perfile.readlines()
+perfile.close()
+
 
 myfirstline = str(mylines[:1])
 myfirstline = myfirstline[2:-5]
@@ -40,8 +68,8 @@ for (counter, item) in enumerate(headernames):
 	print ("{0}.-- {1}".format(counter, item))
 xinput = input ("Select x Axis field number: ")
 yinput = input ("Select y Axis field number: ")
-filternum = input ("Select filter field number: ")
-filtertext = input ("Keep only those values: ")
+filternum = raw_input ("Select filter field number: ")
+filtertext = raw_input ("Keep only those values: ")
 xname = headernames[xinput]
 yname = headernames[yinput]
 
@@ -75,10 +103,10 @@ for line in mylines[1:]:
 	histodico[tmpx] = histodico.get(tmpx, 0) + 1
 	meancumulate[tmpx] = meancumulate.get(tmpx, 0) + tmpy
 
-print (maxdico)
-print (mindico)
-print (histodico)
-print (meancumulate)
+#print (maxdico)
+#print (mindico)
+#print (histodico)
+#print (meancumulate)
 
 maxlist = list(maxdico.items())
 maxlist.sort()
@@ -112,13 +140,35 @@ ax1.set_ylabel(yname)
 ax1.plot(myx, myy, 'o', label="samples")
 ax1.legend(loc="best")
 
+myaxis = input("Change axis ? Type 0 to use actual axis: ({0})".format(ax1.axis()))
+if myaxis:
+	try:
+		ax1.axis(myaxis)
+	except:
+		print ("error setting axis with following parameters ", myaxis)
+
 figure2 = figure()
 ax2 = figure2.add_subplot(111)
 for (counter, item) in enumerate(histolisty):
 	ax2.bar(histolistx[counter], item)
 ax2.set_xlabel(xname)
 ax2.set_ylabel("Samples")
-
 show()
 
-#meantime = {}
+savemean = raw_input("Save mean to file ? (Y/N): ")
+if(savemean.upper() == 'Y'):
+	meanxtxt = [str(x) for x in meanx]
+	meanytxt = [str(x) for x in meany]	
+	savefile = open(homedir + os.sep + "saved-mean.txt", "a")
+	savefile.write("\t".join(meanxtxt) + "\n")
+	mylegendname = ""
+	mylegendresult = "off"
+	try:
+		mylegendname = headernames[int(filternum)]
+	except:
+		mylegendname = "filter"
+	if (filtertext != ""):
+		mylegendresult = filtertext
+	savefile.write("\t".join(meanytxt) + "\tplot({0},{1}, label=\"mean: {2}={3}\")\n".format(meanx, meany, mylegendname, mylegendresult))
+	savefile.close()
+	print ("mean writted to : '{0}'".format(homedir + os.sep + "saved-mean.txt"))
