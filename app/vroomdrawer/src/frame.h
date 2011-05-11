da15f0a741b482fc25ad9455edf8f7e4da28f909
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2011-05-11 14:09:21 +0000

    Adding preliminary code for vroomDrawer sample

---
+++ app/vroomdrawer/src/frame.h
@@ -0,0 +1,98 @@
+/***************************************************************************
+ frame.h
+ Drawer test program for VroomGIS 
+ -------------------
+ copyright            : (C) 2011 CREALP Lucien Schreiber 
+ email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+***************************************************************************/
+
+#ifndef _FRAME_H_
+#define _FRAME_H_
+
+#include "wx/wxprec.h"
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include <wx/filename.h>
+#include <wx/splitter.h>	//splitter bar
+#include "vroomgis.h"
+
+
+const int ID_MENU_SELECT = wxID_HIGHEST+1;
+const int ID_MENU_PAN = wxID_HIGHEST+2;
+const int ID_MENU_ADDMEMORYLAYER = wxID_HIGHEST + 3;
+
+
+class vroomDrawer : public wxApp
+{
+public:
+     virtual bool OnInit();
+};
+DECLARE_APP(vroomDrawer);
+
+
+
+
+class vroomDrawerFrame : public wxFrame
+{
+private:
+	//wxTextCtrl* m_LogCtrl;
+	wxLogWindow * m_LogWnd;
+	
+	// controls (vroomgis)
+	vrViewerTOC * m_TocCtrl;
+	vrViewerDisplay * m_DisplayCtrl;
+	vrLayerManager * m_LayerManager;
+	vrViewerLayerManager * m_ViewerLayerManager;
+
+	wxKeyboardState m_KeyBoardState;
+	wxSlider * m_NbStarCtrl;
+	
+	void _CreateControls();
+	
+	void OnKeyDown(wxKeyEvent & event);
+	void OnKeyUp(wxKeyEvent & event);
+	
+	void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+
+	void OnLayerAdd( wxCommandEvent& event );
+	void OnLayerRemove( wxCommandEvent& event );
+	void OnToolZoom( wxCommandEvent& event );
+	void OnToolSelect( wxCommandEvent& event );
+	void OnToolPan( wxCommandEvent& event ) ;
+	void OnZoomToFit( wxCommandEvent& event ) ;
+	
+	void OnShowLog (wxCommandEvent & event);
+	
+	void OnToolActionZoom (wxCommandEvent & event);
+	void OnToolActionPan (wxCommandEvent & event);
+	void OnToolActionSelect (wxCommandEvent & event);
+    DECLARE_EVENT_TABLE()
+	
+public:
+    vroomDrawerFrame(const wxString& title);
+	~vroomDrawerFrame();
+	
+	bool OpenLayers (const wxArrayString & names);
+};
+
+
+
+
+#endif
+
+
