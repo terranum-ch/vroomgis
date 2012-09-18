dd254f305a7e3b52b003fddb4bec1b6c66dde1c6
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2012-09-18 15:02:52 +0000

    Adding viewer overlay (preliminary code)

---
+++ vroomgis/src/vrvieweroverlay.cpp
@@ -0,0 +1,80 @@
+/***************************************************************************
+ vrvieweroverlay.cpp
+ -------------------
+ copyright            : (C) 2012 CREALP Lucien Schreiber 
+ email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "vrvieweroverlay.h"
+
+
+/*****************************************************************************
+Overlay abstract base class
+*****************************************************************************/
+vrViewerOverlay::vrViewerOverlay(wxString name) {
+    m_Name = name;
+    m_Visible = true;
+}
+
+vrViewerOverlay::~vrViewerOverlay() {
+}
+
+void vrViewerOverlay::SetVisible(bool value) {
+  m_Visible = value;
+}
+
+
+
+
+
+
+
+/***************************************************************************//**
+Overlay simple text class
+*******************************************************************************/
+vrViewerOverlayText::vrViewerOverlayText(wxString name, wxString text) :
+ vrViewerOverlay(name){
+     SetText(text);
+     SetPosition(wxPoint(10,10));
+     m_Font = *wxNORMAL_FONT;
+     m_Font.SetPointSize(m_Font.GetPointSize() + 5);
+}
+
+
+
+vrViewerOverlayText::~vrViewerOverlayText() {
+}
+
+
+
+bool vrViewerOverlayText::DrawOverlay(wxPaintDC * dc) {
+    dc->SetFont(GetFont());
+    dc->DrawText(GetText(), GetPosition());
+    return true;
+}
+
+
+
+void vrViewerOverlayText::SetFont(wxFont value) {
+  m_Font = value;
+}
+
+
+
+void vrViewerOverlayText::SetText(wxString value) {
+  m_Text = value;
+}
+
+void vrViewerOverlayText::SetPosition(wxPoint value) {
+  m_Pos = value;
+}
+
---
+++ vroomgis/src/vrvieweroverlay.h
@@ -0,0 +1,102 @@
+/***************************************************************************
+ vrvieweroverlay.h
+ -------------------
+ copyright            : (C) 2012 CREALP Lucien Schreiber 
+ email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef _VRVIEWEROVERLAY_H_
+#define _VRVIEWEROVERLAY_H_
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+// Include wxWidgets' headers
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+
+/***************************************************************************//**
+@brief Overlay abstract base class
+@author Lucien Schreiber (c) CREALP 2012
+@date 18 september 2012
+*******************************************************************************/
+class vrViewerOverlay {
+private:
+    wxString m_Name;
+    bool m_Visible;
+    
+    
+public:
+    vrViewerOverlay(wxString name);
+    virtual ~vrViewerOverlay();
+    
+    inline const wxString GetName() const;
+    inline const bool IsVisible() const;
+    
+    void SetVisible(bool value);
+    virtual bool DrawOverlay(wxPaintDC * dc) = 0;
+};
+
+inline const wxString vrViewerOverlay::GetName() const {
+    return m_Name;
+}
+
+
+inline const bool vrViewerOverlay::IsVisible() const {
+    return m_Visible;
+}
+
+
+
+
+
+/***************************************************************************//**
+@brief Overlay simple text class
+@author Lucien Schreiber (c) CREALP 2012
+@date 18 septembre 2012
+*******************************************************************************/
+class vrViewerOverlayText : public vrViewerOverlay {
+private:
+    wxFont m_Font;
+    wxString m_Text;
+    wxPoint m_Pos;
+    
+public:
+    vrViewerOverlayText(wxString name, wxString text);
+    virtual ~vrViewerOverlayText();
+    
+    virtual bool DrawOverlay(wxPaintDC * dc);
+
+    inline const wxFont GetFont() const;
+    void SetFont(wxFont value);
+    inline const wxString GetText() const;
+    void SetText(wxString value);
+    inline const wxPoint GetPosition() const;
+    void SetPosition(wxPoint value);
+    
+};
+
+
+inline const wxFont vrViewerOverlayText::GetFont() const {
+    return m_Font;
+}
+
+inline const wxString vrViewerOverlayText::GetText() const {
+    return m_Text;
+}
+
+inline const wxPoint vrViewerOverlayText::GetPosition() const {
+    return m_Pos;
+}
+
+#endif
