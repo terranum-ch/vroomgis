f4a545e6dae0215a5dc891afd6f8b28a11853109
Lucien Schreiber <lucien.schreiber@gmail.com>
2013-04-11 13:07:29 +0000

    Updating build scripts for Windows Updating resource file for apps

--- scripts/windows-7.py
+++ scripts/windows-7.py
@@ -1,4 +1,4 @@
-# VARIABLES FOR BUILDING ON OSX 
+# VARIABLES FOR BUILDING ON WINDOWS 
 
 # Variables to define, see update_vroomgis.py:
 gwxWidgets = "D:\\LIB\\wxWIDGETS-SVN" #wxWIDGETS config
@@ -10,12 +10,12 @@ gDirTestData = "D:\\PRJ\\COLTOPGIS\\test-data"
 gDirCxx = "D:\\LIB\\cxxtest"
 gDirSQLite = "D:\\LIB\\LIB_SQLITE"
 gDirCurl = "D:\\LIB\\LIB_CURL"
-
+gDirGeos = "D:\\LIB\\geos-3.3.7"
 
 # CMAKE SPECIFIC
 gCmakeEnv = "Visual Studio 10"
 gCmakeSpecific = ['-DUSE_MT_LIBRARY:BOOL=1']
-gCmakeSpecific.append("-DSEARCH_GEOS_LIB_PATH:PATH=D:\\LIB\\geos-3.3.0")
+gCmakeSpecific.append("-DSEARCH_GEOS_LIB_PATH:PATH=" + gDirGeos)
 
 
 # PLATEFORM SPECIFIC FUNCTION
@@ -42,3 +42,23 @@ def gBuildCommand(buildtype="Debug", directory = ""):
 	
 	myConfiguration = "/property:Configuration="+ buildtype
 	return ['msbuild', myTarget, myConfiguration]
+
+
+
+def DoPostBuildCommand(binpath):
+	print ("BinPath", binpath)
+	myDll = (os.path.join(gDirGis,"bin","gdal19.dll"), #,os.path.join(gDirGeos,"bin","Debug", "geos.dll"),
+			 os.path.join(gDirGeos,"src","geos_c.dll"),
+			 os.path.join(gDirCurl, "lib", "DLL-Debug","libcurld.dll"),
+			 os.path.join(gDirCurl, "lib", "DLL-Release","libcurl.dll"),
+			 os.path.join(gDirSQLite, "lib", "sqlite3.dll")
+		)
+
+	for dll in myDll:
+		shutil.copyfile(dll, os.path.join(binpath, "Debug", os.path.basename(dll)))
+		if (os.path.exists(os.path.join(binpath, "RelWithDebInfo"))):
+			shutil.copyfile(dll, os.path.join(binpath, "RelWithDebInfo", os.path.basename(dll)))
+
+		print ("copying: ", dll)
+	return True
+
