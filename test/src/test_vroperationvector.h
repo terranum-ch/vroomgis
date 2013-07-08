62d6a658c16ecb1036d3a7186e7586b9657031cf
Lucien Schreiber <lucien.schreiber@terranum.ch>
2013-07-08 11:26:55 +0200

    vrOperationVector is now working

--- test/src/test_vroperationvector.h
+++ test/src/test_vroperationvector.h
@@ -27,6 +27,7 @@
 #include "vroperationvectorprofiler.h"
 #include "vrlayervector.h"
 #include "vrlayerraster.h"
+#include "vrlayermanager.h"
 
 class TEST_vrOperationVector : public CxxTest::TestSuite
 {
@@ -79,7 +80,62 @@ public:
     void testDoProfile () {
         OGRFeature * myFeat = myVector->GetNextFeature(true);
         vrOperationVectorProfiler myProfiler ( myFeat->GetGeometryRef() , myRaster);
-        TS_ASSERT(myProfiler.DoProfile());        
+        TS_ASSERT(myProfiler.DoProfile());
+        TS_ASSERT(myProfiler.GetResultRef()->GetCount() > 0);
+        OGRFeature::DestroyFeature(myFeat);
+    }
+    
+    
+    
+    void testProfileOut () {
+        vrLayerManager myManager;
+        wxFileName myOutPtName (g_TestPath, g_TestProfileOutPt);
+        myManager.Erase(myOutPtName);
+        
+        wxFileName myOutLineName (g_TestPath, g_TestProfileOutLine);
+        myManager.Erase(myOutLineName);
+        
+        vrLayerVectorOGR myOutPt;
+        TS_ASSERT(myOutPt.Create(myOutPtName, wkbPoint));
+        
+        
+        OGRFeature * myFeat = myVector->GetNextFeature(true);
+        vrOperationVectorProfiler myProfiler ( myFeat->GetGeometryRef() , myRaster);
+        TS_ASSERT(myProfiler.DoProfile());
+        int myNumResult = myProfiler.GetResultRef()->GetCount();
+        
+        for (unsigned int i = 0; i< myNumResult; i++) {
+            OGRPoint myPt;
+            TS_ASSERT(myProfiler.GetResultPoint(i, &myPt));
+            TS_ASSERT(myOutPt.AddFeature(&myPt) != wxNOT_FOUND);
+        }
+    
+    
+    vrLayerVectorOGR myOutLine;
+    TS_ASSERT(myOutLine.Create(myOutLineName, wkbLineString));
+    OGRLineString myLine;
+    TS_ASSERT(myProfiler.GetResultLine(&myLine));
+    TS_ASSERT(myOutLine.AddFeature(&myLine) != wxNOT_FOUND);
+    
+    }
+    
+    
+    void testProfileGeometries () {
+        OGRFeature * myFeat = myVector->GetNextFeature(true);
+        vrOperationVectorProfiler myProfiler ( myFeat->GetGeometryRef() , myRaster);
+        TS_ASSERT(myProfiler.DoProfile());
+        //int myNumResult = myProfiler.GetResultRef()->GetCount();
+
+        OGRPoint myPt0;
+        OGRPoint myPtEnd;
+        OGRLineString * myLine = static_cast<OGRLineString*> (myFeat->GetGeometryRef());
+
+        TS_ASSERT(myProfiler.GetResultPoint(0,&myPt0));
+        TS_ASSERT(myPt0.getX() == myLine->getX(0));
+        TS_ASSERT(myPt0.getY() == myLine->getY(0));
+        
+        TS_ASSERT(myProfiler.GetIncrementDistance() > 0);
+              
         OGRFeature::DestroyFeature(myFeat);
     }
     
