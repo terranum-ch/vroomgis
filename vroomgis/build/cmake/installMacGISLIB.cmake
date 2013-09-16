4fd15c4fca74fdbb5a9b49d2a743d4b0af931f20
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2013-09-16 17:14:24 +0200

    Update cmake script for wxWidgets 3.0 and for installing GIS libraries into Bundle (OSX)

--- vroomgis/build/cmake/installMacGISLIB.cmake
+++ vroomgis/build/cmake/installMacGISLIB.cmake
@@ -12,7 +12,7 @@
 SET (LIBNAME "Library")
 SET (BUNDLEPATH "${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_PROJECT_NAME}.app")
 SET (BUNDLELIBPATH "${BUNDLEPATH}/Contents/${LIBNAME}")
-SET (GEOS_VERSION "3.3.8")
+SET (GEOS_VERSION "3.4.2")
 SET (GEOS_C_VERSION "1")
 SET (CURL_VERSION "4")
 #SET (PROJ_VERSION "0.6.6")
@@ -24,29 +24,29 @@ ENDIF(NOT APPLE)
 
 
 ### COPY GEOS ##
-IF(SEARCH_GEOS)
+IF(GEOS_LIBRARIES AND SEARCH_GIS_LIB_PATH)
   ADD_CUSTOM_COMMAND( TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
     COMMAND mkdir -p 	${BUNDLELIBPATH}
     COMMAND cp ${SEARCH_GIS_LIB_PATH}/lib/libgeos.dylib ${BUNDLELIBPATH}/libgeos.dylib
     COMMAND cp ${SEARCH_GIS_LIB_PATH}/lib/libgeos_c.dylib ${BUNDLELIBPATH}/libgeos_c.dylib
     COMMAND install_name_tool -id  @executable_path/../${LIBNAME}/libgeos.dylib ${BUNDLELIBPATH}/libgeos.dylib
     COMMAND install_name_tool -id  @executable_path/../${LIBNAME}/libgeos_c.dylib ${BUNDLELIBPATH}/libgeos_c.dylib
-    COMMAND install_name_tool -change ${SEARCH_GIS_LIB_PATH}/lib/libgeos-${GEOS_VERSION}.dylib @executable_path/../${LIBNAME}/libgeos.dylib ${BUNDLELIBPATH}/libgeos_c.dylib       
+    COMMAND install_name_tool -change libgeos.${GEOS_VERSION}.dylib @executable_path/../${LIBNAME}/libgeos.dylib ${BUNDLELIBPATH}/libgeos_c.dylib       
 
-    COMMAND install_name_tool  -change ${SEARCH_GIS_LIB_PATH}/lib/libgeos_c.1.dylib @executable_path/../${LIBNAME}/libgeos_c.dylib ${BUNDLEPATH}/Contents/MacOS/${CMAKE_PROJECT_NAME}
+    COMMAND install_name_tool  -change libgeos_c.1.dylib @executable_path/../${LIBNAME}/libgeos_c.dylib ${BUNDLEPATH}/Contents/MacOS/${CMAKE_PROJECT_NAME}
     COMMENT "Copying and updating program GEOS libs"
     )
-ENDIF(SEARCH_GEOS)
+ENDIF()
 
 ### COPY GDAL & PROJ ###
-IF(SEARCH_GDAL)
+IF(GDAL_LIBRARIES AND SEARCH_GIS_LIB_PATH)
   ADD_CUSTOM_COMMAND( TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
     COMMAND mkdir -p 	${BUNDLELIBPATH}
     COMMAND cp ${SEARCH_GIS_LIB_PATH}/lib/libgdal.dylib ${BUNDLELIBPATH}
     COMMAND cp ${SEARCH_GIS_LIB_PATH}/lib/libproj.dylib ${BUNDLELIBPATH}
     COMMAND install_name_tool -id  @executable_path/../${LIBNAME}/libgdal.dylib ${BUNDLELIBPATH}/libgdal.dylib
-    COMMAND install_name_tool -change ${SEARCH_GIS_LIB_PATH}/lib/libgeos-${GEOS_VERSION}.dylib @executable_path/../${LIBNAME}/libgeos.dylib ${BUNDLELIBPATH}/libgdal.dylib
-    COMMAND install_name_tool -change ${SEARCH_GIS_LIB_PATH}/lib/libgeos_c.1.dylib @executable_path/../${LIBNAME}/libgeos_c.dylib ${BUNDLELIBPATH}/libgdal.dylib
+    COMMAND install_name_tool -change ${SEARCH_GIS_LIB_PATH}/lib/libgeos.${GEOS_VERSION}.dylib @executable_path/../${LIBNAME}/libgeos.dylib ${BUNDLELIBPATH}/libgdal.dylib
+    COMMAND install_name_tool -change libgeos_c.1.dylib @executable_path/../${LIBNAME}/libgeos_c.dylib ${BUNDLELIBPATH}/libgdal.dylib
     COMMAND install_name_tool -id  @executable_path/../${LIBNAME}/libproj.dylib ${BUNDLELIBPATH}/libproj.dylib
     COMMAND install_name_tool -change ${SEARCH_GIS_LIB_PATH}/lib/libproj.0.dylib @executable_path/../${LIBNAME}/libproj.dylib ${BUNDLELIBPATH}/libgdal.dylib
 
@@ -54,17 +54,22 @@ IF(SEARCH_GDAL)
 
     COMMENT "Copying and updating GDAL libs"
     )
-ENDIF(SEARCH_GDAL)
+ENDIF()
 
 
 # CURL
-IF(SEARCH_CURL_LIB_PATH)
+IF(CURL_LIBRARIES)
+  get_filename_component (CURL_LIBRARIES_PATH ${CURL_LIBRARIES} PATH)
+  IF ("/usr/lib" STREQUAL CURL_LIBRARIES_PATH)
+    return()
+  ENDIF ()
+
   ADD_CUSTOM_COMMAND( TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
     COMMAND mkdir -p 	${BUNDLELIBPATH}
-    COMMAND cp ${SEARCH_CURL_LIB_PATH}/lib/libcurl.dylib ${BUNDLELIBPATH}
+    COMMAND cp ${CURL_LIBRARIES_PATH}/libcurl.dylib ${BUNDLELIBPATH}
     COMMAND install_name_tool -id  @executable_path/../${LIBNAME}/libcurl.dylib ${BUNDLELIBPATH}/libcurl.dylib
-    COMMAND install_name_tool -change ${SEARCH_CURL_LIB_PATH}/lib/libcurl.${CURL_VERSION}.dylib @executable_path/../${LIBNAME}/libcurl.dylib ${BUNDLELIBPATH}/libgdal.dylib		
-    COMMAND install_name_tool -change ${SEARCH_CURL_LIB_PATH}/lib/libcurl.${CURL_VERSION}.dylib @executable_path/../${LIBNAME}/libcurl.dylib ${BUNDLEPATH}/Contents/MacOS/${CMAKE_PROJECT_NAME}
+    COMMAND install_name_tool -change ${CURL_LIBRARIES_PATH}/libcurl.${CURL_VERSION}.dylib @executable_path/../${LIBNAME}/libcurl.dylib ${BUNDLELIBPATH}/libgdal.dylib		
+    COMMAND install_name_tool -change ${CURL_LIBRARIES_PATH}/libcurl.${CURL_VERSION}.dylib @executable_path/../${LIBNAME}/libcurl.dylib ${BUNDLEPATH}/Contents/MacOS/${CMAKE_PROJECT_NAME}
     COMMENT "Copying and updating CURL libs"
     )
-ENDIF(SEARCH_CURL_LIB_PATH)
+ENDIF(CURL_LIBRARIES)
