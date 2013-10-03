7de31694c015c0b223fb5df9887e06a471eaafce
Lucien Schreiber <lucien.schreiber@terranum.ch>
2013-10-03 15:49:40 +0200

    * Correcting crash report for Windows  * Copying CURL DLL to directory

--- plugins/lscrashreport/build/CURLCopyLibs.cmake
+++ plugins/lscrashreport/build/CURLCopyLibs.cmake
@@ -1,18 +1,28 @@
 IF(WIN32)
 		# copy DLL 
-		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
-          DESTINATION "${PROJECT_BINARY_DIR}/Release"
-          CONFIGURATIONS Release
-        )
-		
-		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
-          DESTINATION "${PROJECT_BINARY_DIR}/RelWithDebInfo"
-          CONFIGURATIONS RelWithDebInfo
-        )
-		
-		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Debug/libcurld.dll"
-          DESTINATION "${PROJECT_BINARY_DIR}/Debug"
-          CONFIGURATIONS Debug
-        )
+#		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
+#          DESTINATION "${PROJECT_BINARY_DIR}/Release"
+#          CONFIGURATIONS Release
+#        )
+#		
+#		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
+#          DESTINATION "${PROJECT_BINARY_DIR}/RelWithDebInfo"
+#          CONFIGURATIONS RelWithDebInfo
+#        )
+#		
+#		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Debug/libcurld.dll"
+#          DESTINATION "${PROJECT_BINARY_DIR}/Debug"
+#          CONFIGURATIONS Debug
+#        )
+
+	add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different
+		"${CURL_INCLUDE_DIR}/../lib/DLL-Debug/libcurld.dll"
+		"${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
+	
+	add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
+		"${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
 		
 	ENDIF(WIN32)
