d9bd79586a2aab23e97a3135b0fcad7757c915e2
Lucien Schreiber <lucien.schreiber@terranum.ch>
2013-10-10 14:27:08 +0200

    Improving copy DLL cmake scripts for GDAL and CURL

--- plugins/lscrashreport/build/CURLCopyLibs.cmake
+++ plugins/lscrashreport/build/CURLCopyLibs.cmake
@@ -1,28 +1,29 @@
 IF(WIN32)
-		# copy DLL 
-#		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
-#          DESTINATION "${PROJECT_BINARY_DIR}/Release"
-#          CONFIGURATIONS Release
-#        )
-#		
-#		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
-#          DESTINATION "${PROJECT_BINARY_DIR}/RelWithDebInfo"
-#          CONFIGURATIONS RelWithDebInfo
-#        )
-#		
-#		install(FILES "${CURL_INCLUDE_DIR}/../lib/DLL-Debug/libcurld.dll"
-#          DESTINATION "${PROJECT_BINARY_DIR}/Debug"
-#          CONFIGURATIONS Debug
-#        )
+  # curl library debug
+  FIND_FILE (CURL_DLL_NAME_DEBUG
+    libcurld.dll
+    HINTS "${CURL_INCLUDE_DIR}/../lib/DLL-Debug"
+    NO_DEFAULT_PATH)
+  IF (NOT CURL_DLL_NAME_DEBUG)
+    MESSAGE (SEND_ERROR "libcurld.dll (debug) not found in ${CURL_INCLUDE_DIR}")
+  ENDIF()
 
-	add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy_if_different
-		"${CURL_INCLUDE_DIR}/../lib/DLL-Debug/libcurld.dll"
-		"${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
-	
-	add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy_if_different
-    "${CURL_INCLUDE_DIR}/../lib/DLL-Release/libcurl.dll"
-		"${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
-		
-	ENDIF(WIN32)
+  add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    ${CURL_DLL_NAME_DEBUG}
+    "${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
+
+  # curl library release
+  FIND_FILE (CURL_DLL_NAME
+    libcurl.dll
+    HINTS "${CURL_INCLUDE_DIR}/../lib/DLL-Release"
+    NO_DEFAULT_PATH)
+  IF (NOT CURL_DLL_NAME)
+    MESSAGE (SEND_ERROR "libcurld.dll (debug) not found in ${CURL_INCLUDE_DIR}")
+  ENDIF()
+
+  add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    ${CURL_DLL_NAME}
+    "${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
+ENDIF(WIN32)
